5cf288d84491fd16ed6db52e28a37f68bd0d54ab
Bisal77 <bisal.sahu1997@gmail.com>
2023-04-04 20:59:37 +0530

    adding new

---
+++ dom.js
@@ -0,0 +1,10 @@
+
+console.log(document.getElementById('header-title'));
+var header = document.getElementById('main-header');
+
+header.style.borderBottom = 'solid 4px #000';
+
+
+var confuse = document.getElementById('conf');
+confuse.innerHTML = '<h1>Add Items</h1>'; 
+confuse.style.color = "green";
