922f2c179b6789b2247f085066c6c43cb4c3bb37
Bisal77 <129731861+Bisal77@users.noreply.github.com>
2023-04-05 14:28:00 +0530

    Add files via upload
    Dom part 3

---
+++ dom3.js
@@ -0,0 +1,8 @@
+var added = document.getElementsByClassName('added');
+added[0].style.fontWeight = 'bold';
+added[0].style.backgroundColor = 'blue';
+
+var li = document.getElementsByTagName('li');
+for(var i=0; i<li.length; i++){
+    li[i].style.color = '#D2691E';
+}
